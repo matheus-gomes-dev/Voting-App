317ddf9569ffb066ba86d1a1dfafd6f529ed7770
matheus-lima92 <lima.eca@gmail.com>
2018-03-01 23:04:32 -0300

    removed atlas credentials

--- config/config.js
+++ config/config.js
@@ -5,11 +5,6 @@ const config = {
 			db: "mongodb://localhost:27017/votingApp",
 			username: "",
 			pass: ""
-		},
-		atlas: {
-			db: 'mongodb://Matheus:kirkhetfield_92@cluster0-shard-00-00-qrgyy.mongodb.net:27017,cluster0-shard-00-01-qrgyy.mongodb.net:27017,cluster0-shard-00-02-qrgyy.mongodb.net:27017/alumni?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
-			username: "",
-			pass: ""
 		}
 	},
 	passport:{
